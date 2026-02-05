7092c8b14c18bc2a1bb4f2f11295b2c289cef858
deivydjs <dsantirodriguez12@gmail.com>
2026-02-05 11:37:52 -0500

    Correccion Dockerizado

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,26 +1,44 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
-import checker from 'vite-plugin-checker';
+// import { defineConfig } from 'vite';
+// import react from '@vitejs/plugin-react';
+// import tsconfigPaths from 'vite-tsconfig-paths';
+// import checker from 'vite-plugin-checker';
 
-// https://vitejs.dev/config/
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     tsconfigPaths(),
+//     react(),
+//     checker({
+//       typescript: true,
+//       eslint: {
+//         lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+//       },
+//     }),
+//   ],
+//   preview: {
+//     port: 5000,
+//   },
+//   server: {
+//     host: '0.0.0.0',
+//     port: 3000,
+//   },
+//   base: '/',
+// });
+
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
-  plugins: [
-    tsconfigPaths(),
-    react(),
-    checker({
-      typescript: true,
-      eslint: {
-        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-      },
-    }),
-  ],
-  preview: {
-    port: 5000,
-  },
+  plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: 3000,
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true, // Necesario si usas Windows o notas que no se actualiza solo
+    },
+    hmr: {
+        host: 'localhost',
+    },
   },
-  base: '/',
-});
+})
