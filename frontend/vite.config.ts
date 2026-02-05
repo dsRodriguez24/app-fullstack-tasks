9382fff40b9c52ddf5d64cc6edde00773e920a72
deivydjs <dsantirodriguez12@gmail.com>
2026-02-05 14:13:58 -0500

    Ajuste de tsconfig y vite para docker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,44 +1,32 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-// import tsconfigPaths from 'vite-tsconfig-paths';
-// import checker from 'vite-plugin-checker';
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     tsconfigPaths(),
-//     react(),
-//     checker({
-//       typescript: true,
-//       eslint: {
-//         lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
-//       },
-//     }),
-//   ],
-//   preview: {
-//     port: 5000,
-//   },
-//   server: {
-//     host: '0.0.0.0',
-//     port: 3000,
-//   },
-//   base: '/',
-// });
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
+import { fileURLToPath } from 'url'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "providers": path.resolve(__dirname, "./src/providers"),
+      "api": path.resolve(__dirname, "./src/api"),
+      "helpers": path.resolve(__dirname, "./src/helpers"),
+      "routes": path.resolve(__dirname, "./src/routes"),
+      "theme": path.resolve(__dirname, "./src/theme"),
+      "store": path.resolve(__dirname, "./src/store"),
+      "layouts": path.resolve(__dirname, "./src/layouts"),
+      "pages": path.resolve(__dirname, "./src/pages"),
+      "components": path.resolve(__dirname, "./src/components"),
+      "App": path.resolve(__dirname, "./src/App"),
+    }
+  },
   server: {
     host: true,
     port: 5173,
     watch: {
-      usePolling: true, // Necesario si usas Windows o notas que no se actualiza solo
-    },
-    hmr: {
-        host: 'localhost',
+      usePolling: true,
     },
   },
 })
